d60bb505ef1df19e5cefab2c1573fdccf0cd782c
Manchini1337 <mezureno@gmail.com>
2022-09-12 14:03:20 +0200

    cors options

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 
 // Db connection
 // db.sync({ force: true })
